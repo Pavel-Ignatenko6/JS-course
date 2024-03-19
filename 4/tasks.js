4b1273d3dbb01393aeb6a4e17ece320a6b32fcbd
[Pavel Ignatenko] <[pavel.ignatenko1996@gmail.com]>
2024-03-19 20:28:52 +0800

    add the last 2 tasks

--- 4/tasks.js
+++ 4/tasks.js
@@ -77,7 +77,28 @@ export function sumArrElems(arr) {
  */
 export function binarySearchIndex(arr, num) {
   // your implementation here
+
+  let start = 0;
+  let end = arr.length - 1;
+  while (start <= end) {
+    let middle = Math.ceil((start + end) / 2);
+    if (arr[middle] < num) {
+      // Search the right half
+      start = middle + 1;
+    } else if (arr[middle] > num) {
+      // Search the left half
+      end = middle - 1;
+    } else if (arr[middle] === num) {
+      // Found num
+      return middle;
+    }
+  }
+  // Target not found
+  return -1;
 }
+
+console.log(binarySearchIndex([1, 2, 10, 44, 105, 166, 207, 408, 900], 10)); // 2
+console.log(binarySearchIndex([100, 203, 305, 344, 500, 565, 587, 800], 344)); // 3
 
 /**
  * Write an implementation of the 'spiralOrder' function
@@ -104,4 +125,78 @@ export function binarySearchIndex(arr, num) {
  */
 export function spiralOrder(matrix) {
   // your implementation here
+
+  // вариант 1 - нашёл такое решение, но сложно понять
+  // let result = []
+
+  // while (matrix.length) {
+  //   result.push(
+  //     ...matrix.shift(),
+  //     ...matrix.map(a => a.pop()),
+  //     ...(matrix.pop() || []).reverse(),
+  //     ...matrix.map(a => a.shift()).reverse()
+  //   );
+  // }
+  // return result;
+
+  // console.log(
+  //   spiralOrder([
+  //     [1, 2, 3, 4, 5],
+  //     [6, 7, 8, 9, 10],
+  //     [11, 12, 13, 14, 15],
+  //     [16, 17, 18, 19, 20],
+  //   ])
+  // );
+
+  // вариант 2 - нашёл в интернете. Думаю, лучше обсудить на уроке
+
+  let result = [];
+
+  const readLayer = (arr) => {
+    if (arr.length === 0) {
+      return;
+    }
+
+    if (arr.length === 1) {
+      result = result.concat(arr[0]);
+      return;
+    }
+
+    // step 1.) read and remove topmost row
+    for (let i = 0; i < arr[0].length; i++) {
+      result.push(arr[0][i]);
+    }
+    arr.shift();
+
+    // step 2.) log and remove last value from each row (downward)
+    for (let i = 0; i < arr.length - 1; i++) {
+      result.push(arr[i][arr[i].length - 1]);
+      arr[i].pop();
+      if (arr[i].length === 0) {
+        arr.splice(i, 1);
+        i--;
+      }
+    }
+
+    // step 3.) read and remove lowest row (in reverse)
+    for (let i = arr[arr.length - 1].length - 1; i >= 0; i--) {
+      result.push(arr[arr.length - 1][i]);
+    }
+    arr.pop();
+
+    // step 4.) log and remove first value from each row (upward)
+    for (let i = arr.length - 1; i >= 0; i--) {
+      result.push(arr[i][0]);
+      arr[i].shift();
+      if (arr[i].length === 0) {
+        arr.splice(i, 1);
+      }
+    }
+
+    // repeat with truncated matrix
+    readLayer(arr);
+  };
+
+  readLayer(matrix);
+  return result;
 }
