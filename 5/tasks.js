f8b4711582e3d1cab80b67c3e5542da431171cc1
Pavel Ignatenko <pavel.ignatenko1996@gmail.com>
2024-04-23 17:49:16 +0800

    add task5

--- 5/tasks.js
+++ 5/tasks.js
@@ -8,4 +8,28 @@
  */
 export function isPalindrome(str) {
   // your implementation here
+  let changedStr = ''
+  let backwardsStr = ""
+  let specialChars = /[`!@#$%^&*()_\-+=\[\]{};'’:"\\|,.<>\/?~ ]/;
+
+
+  for (let i = 0; i < str.length; i++) {
+    if (str[i] !== " " && !specialChars.test(str[i])) {
+      changedStr += str[i];
+    }
+  }
+
+  for (let i = changedStr.length - 1; i >= 0; i--) {
+    backwardsStr += changedStr[i]
+  }
+
+  if (changedStr.toLowerCase().trim() === backwardsStr.toLowerCase().trim()) {
+    return true;
+  }
+  return false
 }
+
+
+console.log(isPalindrome("Hello"));
+console.log(isPalindrome("Yo, Banana Boy! "));
+// console.log(isPalindrome("radar"));
